f219bffa00c383f3ec88667ffef26f97d150a8e4
weizhijun <weizhijun@hxquan.cn>
2017-08-31 20:57:17 +0800

    添加加载包逻辑

--- smartcore/src/main/java/com/wzj/util/ClassUtil.java
+++ smartcore/src/main/java/com/wzj/util/ClassUtil.java
@@ -3,8 +3,14 @@ package com.wzj.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 public class ClassUtil {
 
@@ -32,6 +38,7 @@ public class ClassUtil {
             aClass = Class.forName(className, initialize, getClassLoader());
         } catch (ClassNotFoundException e) {
             logger.error("加载类出错", e);
+            throw new RuntimeException(e);
         }
         return aClass;
     }
@@ -44,7 +51,43 @@ public class ClassUtil {
      */
     public static Set<Class<?>> getClassSet(String packageName) {
         Set<Class<?>> classSet = new HashSet<>();
+        ClassLoader classLoader = getClassLoader();
+        try {
+            Enumeration<URL> urlEnumeration = classLoader.getResources(packageName.replace(".", "/"));
+            while (urlEnumeration.hasMoreElements()) {
+                URL url = urlEnumeration.nextElement();
+                if (url != null) {
+                    String protocol = url.getProtocol();
+                    if ("file".equals(protocol)) {
+                        String packagePath = url.getPath().replace("%20", "");
+                        // TODO: 2017/8/31 从包下取class
+                    } else if ("jar".equals(protocol)) {
+                        JarURLConnection jarURLConnection = (JarURLConnection) url.openConnection();
+                        if (jarURLConnection != null) {
+                            JarFile jarFile = jarURLConnection.getJarFile();
+                            Enumeration<JarEntry> jarEntryEnumeration = jarFile.entries();
+                            while (jarEntryEnumeration.hasMoreElements()){
+                                JarEntry jarEntry = jarEntryEnumeration.nextElement();
+                                String jarEntryName = jarEntry.getName();
+                                if (jarEntryName.endsWith(".class")){
+                                    String className = jarEntryName.substring(0, jarEntryName.lastIndexOf(".")).replaceAll("/", ".");
+                                } else {
 
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            logger.error("加载包出错", e);
+            throw new RuntimeException(e);
+        }
         return classSet;
+    }
+
+    public static void doLoadClass(Set<Class<?>> classSet, String className){
+        Class<?> aClass = loadClass(className, false);
+        classSet.add(aClass);
     }
 }
